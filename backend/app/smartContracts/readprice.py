65173261bcedb98d4a91a8476f3c14c1e61171f6
Gere_X <52193900+Gere321123@users.noreply.github.com>
2024-12-04 16:01:01 +0100

    working

--- backend/app/smartContracts/readprice.py
+++ backend/app/smartContracts/readprice.py
@@ -8,6 +8,9 @@ import threading
 # Initialize Flask app
 app = Flask(__name__)
 
+import requests
+
+
 
 CONTRACT_ABI = [
     {
@@ -20,6 +23,16 @@ CONTRACT_ABI = [
 ]
 # Database connection
 DATABASE = "cryptostock.db"
+
+def get_bitcoin_value(btc_amount):
+    # Fetch the current Bitcoin price in USD
+    response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+    data = response.json()
+    btc_price = data['bitcoin']['usd']
+    
+    # Calculate the USD value of the given Bitcoin amount
+    usd_value = btc_amount * btc_price
+    return usd_value
 
 # Function to update stock prices in the database
 def update_stock_prices():
@@ -49,20 +62,17 @@ def update_stock_prices():
             try:
                 # Call the contract function
                 token_value_in_wei = contract.functions.getValueOfOneTokenInWei().call()
-
                 # Convert Wei to Bitcoin (1 Bitcoin = 10**18 Wei)
-                token_value_in_bitcoin = Web3.fromWei(token_value_in_wei, "ether")
-
+                token_value_in_bitcoin = token_value_in_wei / (10 **18)
                 # Calculate the token value in USD
-                bitcoin_price_usd = get_bitcoin_price()
-                token_value_in_usd = token_value_in_bitcoin * bitcoin_price_usd
+                token_value_in_usd = get_bitcoin_value(token_value_in_bitcoin)
 
                 # Update price and priceinUSD in the database
                 cursor.execute(
                     "UPDATE stock SET price = ?, priceinUSD = ? WHERE id = ?",
                     (token_value_in_bitcoin, token_value_in_usd, stock_id),
                 )
-                print(f"Updated stock ID {stock_id}: price = {token_value_in_bitcoin}, priceinUSD = {token_value_in_usd}")
+                #print(f"Updated stock ID {stock_id}: price = {token_value_in_bitcoin}, priceinUSD = {token_value_in_usd}")
 
             except Exception as e:
                 print(f"Error reading contract for stock ID {stock_id}: {e}")
--- frontend/crypto_stock/src/components/Stocks/TransactionSection.vue
+++ frontend/crypto_stock/src/components/Stocks/TransactionSection.vue
@@ -71,7 +71,7 @@ export default defineComponent({
     const connectComponent = ref();
     const { data, isError, isLoading, error } = useReadContract({
     abi,
-    address: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
+    address: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
     functionName: 'getValueOfOneTokenInWei',
     config: config
   });
