bd354d1d76ec4ae4dcbde79ef7ba72e8f4d4fe5c
Gere_X <52193900+Gere321123@users.noreply.github.com>
2024-12-17 13:56:12 +0100

    good for mobil

--- frontend/crypto_stock/src/components/NavbarComponent.vue
+++ frontend/crypto_stock/src/components/NavbarComponent.vue
@@ -1,21 +1,44 @@
 <template>
   <nav class="navbar">
     <div class="navbar-logo">Crypto Stock</div>
-    <ul class="navbar-links">
-      <li><router-link to="/">Home</router-link></li>
-      <li><router-link to="/buy-sell">Buy/Sell</router-link></li>
-      <li><router-link to="/for-companies">For Companies</router-link></li>
+
+    <!-- Hamburger Icon -->
+    <div class="hamburger" @click="toggleMenu">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+
+    <!-- Navigation Links -->
+    <ul class="navbar-links" :class="{ active: isMenuOpen }">
+      <li><router-link to="/" @click="closeMenu">Home</router-link></li>
+      <li><router-link to="/buy-sell" @click="closeMenu">Buy/Sell</router-link></li>
+      <li><router-link to="/for-companies" @click="closeMenu">For Companies</router-link></li>
     </ul>
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'NavbarComponent'
-}
+  name: "NavbarComponent",
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+    closeMenu() {
+      this.isMenuOpen = false;
+    },
+  },
+};
 </script>
 
 <style scoped>
+/* Main Navbar Styles */
 .navbar {
   display: flex;
   align-items: center;
@@ -32,6 +55,23 @@ export default {
   cursor: pointer;
 }
 
+/* Hamburger Menu */
+.hamburger {
+  display: none;
+  flex-direction: column;
+  cursor: pointer;
+  gap: 5px;
+}
+
+.hamburger div {
+  width: 25px;
+  height: 3px;
+  background-color: #ffffff;
+  border-radius: 4px;
+  transition: all 0.3s ease;
+}
+
+/* Navbar Links */
 .navbar-links {
   list-style-type: none;
   display: flex;
@@ -55,5 +95,42 @@ export default {
 .navbar-links a:hover {
   color: #ffffff;
   background-color: #3a3b3e;
+}
+
+/* Responsive Styles */
+@media screen and (max-width: 768px) {
+  .hamburger {
+    display: flex;
+  }
+
+  .navbar-links {
+    display: none;
+    flex-direction: column;
+    background-color: #232427;
+    position: absolute;
+    top: 60px;
+    right: 0;
+    width: 100%;
+    padding: 10px 0;
+    text-align: center;
+    gap: 15px;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.5);
+    z-index: 10;
+  }
+
+  .navbar-links.active {
+    display: flex;
+  }
+
+  .navbar-links li {
+    margin: 0;
+  }
+
+  .navbar-links a {
+    display: block;
+    width: 100%;
+    padding: 12px;
+    color: #ffffff;
+  }
 }
 </style>
