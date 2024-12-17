8dcc2b5686ef540169305f3f4430a1634b36902b
Gere_X <52193900+Gere321123@users.noreply.github.com>
2024-12-17 13:51:10 +0100

    gap

--- frontend/crypto_stock/src/components/Stocks/BuySellComponent.vue
+++ frontend/crypto_stock/src/components/Stocks/BuySellComponent.vue
@@ -21,14 +21,15 @@
 
       <!-- Stock List -->
       <h3>Existing Stocks</h3>
-      <ul class="company-list" @click="handleContainerClick">
+      <ul class="company-list">
   <li 
     v-for="stock in filteredStocks" 
     :key="stock[0]" 
     @click.stop="goToCompanyDetail(stock[0])" 
     class="company-item"
   >
-    {{ stock[2] }} - {{ stock[3] }} - ${{ stock[17] }}
+  <span>{{ stock[2] }}</span>
+  <span>{{ stock[17] }}$</span>
     <button @click.stop="goToCompanyDetail(stock[0])" class="learn-more-button">
       Learn More
     </button>
